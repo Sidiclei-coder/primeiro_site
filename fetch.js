46e0ed4745408460df6f25e01bf581c04825aace
Sidiclei <54724231+Sidiclei-coder@users.noreply.github.com>
2020-05-12 03:39:35 -0300

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 window.onload = async function(){
-    const response = await fetch("https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72");
+    const response = await this.fetch("https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72");
     const quartos = await response.json();
 
     let nomes = quartos.map(quartos => `<div class="titulo"><h6 class="card-title">${quartos.name[0].toUpperCase() + quartos.name.slice(1).toLowerCase()}</h6> </div>`);
